90a49d077027adc1de1b8aa86ddf1394f89d75dd
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-10-24 16:19:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,9 @@
 var playlist = { 
-  artistName: "songtitle",
-  artistName: "songtitle",
+  artistName: "songTitle",
   
 }
-function updatePlaylist (playlist, "Kent", "Sverige") {
-  playlist[Kent] = "Sverige";
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist[artistName] = "songTitle";
   return playlist;
 }
 
