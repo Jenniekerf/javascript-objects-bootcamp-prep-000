ad9de40fa47dd6f2a44f8f210260ee689b2d6569
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-10-24 16:13:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { 
   Madonna: "Like a virgin",
   Prince: "Get up",
-  U2: "Where the streets have no name",
+  
 }
 function updatePlaylist (playlist, "Kent", "Sverige") {
   playlist[Kent] = "Sverige";
